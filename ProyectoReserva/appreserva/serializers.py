984000222392f23ebdc375b7cc958d21aa3f0cf3
Rafael <Ra.felix@duocuc.cl>
2024-06-17 21:44:24 -0400

    cambio para presentar

--- ProyectoReserva/appreserva/serializers.py
+++ ProyectoReserva/appreserva/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Cliente, Cancha, DisponibilidadCancha, Reserva, Pago
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']  
 
 class ClienteSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Cliente
-        fields = '__all__'
+        fields = ['id', 'user', 'telefono', 'direccion']
 
 class CanchaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,12 +24,12 @@ class DisponibilidadCanchaSerializer(serializers.ModelSerializer):
         model = DisponibilidadCancha
         fields = '__all__'
 
-class PagoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pago
-        fields = '__all__'
-
 class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
+        fields = '__all__'
+
+class PagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pago
         fields = '__all__'
--- ProyectoReserva/appreserva/views.py
+++ ProyectoReserva/appreserva/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Cliente, Cancha, DisponibilidadCancha, Reserva, Pago
-from .serializers import ClienteSerializer, CanchaSerializer, DisponibilidadCanchaSerializer, ReservaSerializer, PagoSerializer
+from .serializers import (
+    ClienteSerializer, CanchaSerializer, DisponibilidadCanchaSerializer,
+    ReservaSerializer, PagoSerializer
+)
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
@@ -14,10 +17,10 @@ class DisponibilidadCanchaViewSet(viewsets.ModelViewSet):
     queryset = DisponibilidadCancha.objects.all()
     serializer_class = DisponibilidadCanchaSerializer
 
-class PagoViewSet(viewsets.ModelViewSet):
-    queryset = Pago.objects.all()
-    serializer_class = PagoSerializer
-
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializer
+
+class PagoViewSet(viewsets.ModelViewSet):
+    queryset = Pago.objects.all()
+    serializer_class = PagoSerializer
